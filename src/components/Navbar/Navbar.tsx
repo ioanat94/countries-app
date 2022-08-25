800b40d4968db2e186b283ef532155941e70c914
Ioana Tiplea <ioanatiplea94@gmail.com>
2022-08-25 19:49:38 +0300

    Add link to Home page

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import Favorites from '../Favorites/Favorites'
 import Search from '../Search/Search'
@@ -7,14 +8,14 @@ function Navbar() {
   return (
     <div className='h-40 flex items-center justify-between bg-darkerBlue md:h-20 px-6'>
       <div className='flex flex-col gap-4 md:flex-row md:gap-8 md:items-center'>
-        <div className='flex items-center gap-4 md:gap-8'>
+        <Link to='/' className='flex items-center gap-4 md:gap-8'>
           <img
             src={require('../../assets/globe.png')}
             alt='Website Logo'
             className='w-12'
           />
           <h1 className='text-white text-2xl'>Countries</h1>
-        </div>
+        </Link>
         <Search />
       </div>
       <Favorites />
