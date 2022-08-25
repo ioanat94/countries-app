3f12911200a420d3ba90092279e2db2d27e6f65f
Ioana Tiplea <ioanatiplea94@gmail.com>
2022-08-25 19:09:21 +0300

    Add dummy data

---
+++ src/dummyData.ts
@@ -0,0 +1,39 @@
+const dummyData = [
+  {
+    flag: 'https://flagcdn.com/w320/fi.png',
+    name: 'Finland',
+    languages: ['Finnish', 'Swedish'],
+    population: 5530719,
+    region: 'Europe',
+  },
+  {
+    flag: 'https://flagcdn.com/w320/fi.png',
+    name: 'Finland',
+    languages: ['Finnish', 'Swedish'],
+    population: 5530719,
+    region: 'Europe',
+  },
+  {
+    flag: 'https://flagcdn.com/w320/fi.png',
+    name: 'Finland',
+    languages: ['Finnish', 'Swedish'],
+    population: 5530719,
+    region: 'Europe',
+  },
+  {
+    flag: 'https://flagcdn.com/w320/fi.png',
+    name: 'Finland',
+    languages: ['Finnish', 'Swedish'],
+    population: 5530719,
+    region: 'Europe',
+  },
+  {
+    flag: 'https://flagcdn.com/w320/fi.png',
+    name: 'Finland',
+    languages: ['Finnish', 'Swedish'],
+    population: 5530719,
+    region: 'Europe',
+  },
+]
+
+export default dummyData
