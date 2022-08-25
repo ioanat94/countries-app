9f677a86fb389dbcc35c0a19e09aabc73bfe0e1a
Ioana Tiplea <ioanatiplea94@gmail.com>
2022-08-25 19:10:46 +0300

    Create Table component

---
+++ src/components/Table/Table.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import TableHead from '../TableHead/TableHead'
+import dummyData from '../../dummyData'
+import TableRow from '../TableRow/TableRow'
+
+function Table() {
+  return (
+    <div className='px-10 py-10 overflow-auto xl:px-20'>
+      <table className='border-collapse w-full'>
+        <TableHead />
+        <tbody className='text-greyishBlueLight'>
+          {dummyData.map((data) => (
+            <TableRow data={data} />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+export default Table
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../components/Navbar/Navbar'
+import Table from '../components/Table/Table'
 
 function Home() {
   return (
     <div className='h-screen bg-darkBlue'>
       <Navbar />
+      <Table />
     </div>
   )
 }
