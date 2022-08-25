1d6b42b7451b556f62afdfca2a25eac26fc63027
Ioana Tiplea <ioanatiplea94@gmail.com>
2022-08-25 17:36:57 +0300

    Change Prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,9 @@
 module.exports = {
-  trailingComma: "es5",
+  trailingComma: 'es5',
   tabWidth: 2,
   useTabs: false,
   jsxBracketSameLine: false,
   semi: false,
-  singleQuote: true
+  singleQuote: true,
+  jsxSingleQuote: true,
 }
