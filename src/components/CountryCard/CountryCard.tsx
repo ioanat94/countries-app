372be78b4d79535fb65487b55fbb89f557607b57
Ioana Tiplea <ioanatiplea94@gmail.com>
2022-08-25 19:47:34 +0300

    Create CountryCard component

---
+++ src/components/CountryCard/CountryCard.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import AddFavoriteBtn from '../AddFavoriteBtn/AddFavoriteBtn'
+import CountryInfo from '../CountryInfo/CountryInfo'
+
+function CountryCard() {
+  return (
+    <div className='flex items-center justify-center h-full'>
+      <div className='flex flex-col items-center bg-greyishBlue mx-4 px-20 pt-20 pb-10 rounded-xl drop-shadow-2xl'>
+        <CountryInfo />
+        <AddFavoriteBtn />
+      </div>
+    </div>
+  )
+}
+export default CountryCard
