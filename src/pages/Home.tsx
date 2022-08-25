ac0a54d72b98b353251745d71eeeb9c2f6b14bcf
Ioana Tiplea <ioanatiplea94@gmail.com>
2022-08-25 17:39:37 +0300

    Create Home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../components/Navbar/Navbar'
+
+function Home() {
+  return (
+    <div className='h-screen bg-darkBlue'>
+      <Navbar />
+    </div>
+  )
+}
+
+export default Home
