adb2a06f10c82f14559222d9c5ec191d466f180a
Ioana Tiplea <ioanatiplea94@gmail.com>
2022-08-25 19:10:14 +0300

    Create AddFavoriteBtn component

---
+++ src/components/AddFavoriteBtn/AddFavoriteBtn.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function AddFavoriteBtn() {
+  return (
+    <button className='bg-greyishBlue text-darkBlue font-bold rounded px-4 py-2 border-2 border-greyishBlue md:hover:bg-darkBlue md:hover:text-greyishBlue md:transition-all'>
+      ADD
+    </button>
+  )
+}
+export default AddFavoriteBtn
