ce9b61a39fd3ff44579a4a87b24cc5a031374c4f
Ioana Tiplea <ioanatiplea94@gmail.com>
2022-08-25 19:47:50 +0300

    Create Country page

--- src/pages/Country.tsx
+++ src/pages/Country.tsx
@@ -1,6 +1,14 @@
 import React from 'react'
 
+import CountryCard from '../components/CountryCard/CountryCard'
+import Navbar from '../components/Navbar/Navbar'
+
 function Country() {
-  return <div>Country</div>
+  return (
+    <div className='h-screen bg-darkBlue'>
+      <Navbar />
+      <CountryCard />
+    </div>
+  )
 }
 export default Country
