c96900d8934046aa5e9632cbba286bd3f56ccf1b
Ioana Tiplea <ioanatiplea94@gmail.com>
2022-08-26 12:14:24 +0300

    Minor style changes

--- src/components/FavoritesSlider/FavoritesSlider.tsx
+++ src/components/FavoritesSlider/FavoritesSlider.tsx
@@ -5,7 +5,7 @@ import FavoritesRow from '../FavoritesRow/FavoritesRow'
 
 function FavoritesSlider() {
   return (
-    <div className='bg-darkerBlue text-2xl px-6 h-full min-w-full md:min-w-[400px] fixed top-0 right-0 translate-x-[100vw] md:translate-x-[400px] translate-x-[0]'>
+    <div className='bg-darkerBlue text-2xl px-6 h-full min-w-full fixed top-0 right-0 invisible md:min-w-[400px]'>
       <FavoritesHeader />
       <FavoritesRow />
       <FavoritesRow />
