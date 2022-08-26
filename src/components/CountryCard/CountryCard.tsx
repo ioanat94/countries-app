61a7296885486b9d7ef5612d2963e3e227d749ec
Ioana Tiplea <ioanatiplea94@gmail.com>
2022-08-26 17:25:49 +0300

    Minor style changes

--- src/components/CountryCard/CountryCard.tsx
+++ src/components/CountryCard/CountryCard.tsx
@@ -10,11 +10,11 @@ function CountryCard() {
 
   return (
     // Wrapping divs to center card on page
-    <div className='flex items-center justify-center h-full'>
+    <div className='flex items-center justify-center pt-14 md:h-screen md:pt-0'>
       <div
         className={`${
           theme === 'dark' ? 'bg-greyishBlue' : 'bg-darkBlue'
-        } flex flex-col items-center mx-4 px-20 pt-20 pb-10 rounded-xl drop-shadow-2xl`}
+        } flex flex-col items-center min-w-[90vw] mx-4 px-20 py-5 rounded-xl drop-shadow-2xl md:min-w-[50vw] md:pt-20 md:pb-10`}
       >
         <CountryInfo />
         <AddFavoriteBtn />
