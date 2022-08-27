381cdf5aeb8e4691054ad3b1ba54cc1a10deec5f
Ioana Tiplea <ioanatiplea94@gmail.com>
2022-08-27 14:11:59 +0300

    Make sure navigating to Home page refreshes

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
+import { Refreshable } from 'react-router-refreshable'
 
 import Country from './pages/Country'
 import Home from './pages/Home'
 
 const Routes = () => (
-  <Switch>
-    <Route exact path='/' component={Home} />
-    <Route exact path='/countries/:param' component={Country} />
-  </Switch>
+  <Refreshable>
+    <Switch>
+      <Route exact path='/' component={Home} />
+      <Route exact path='/countries/:param' component={Country} />
+    </Switch>
+  </Refreshable>
 )
 
 export default Routes
