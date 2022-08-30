cb9c4ba97ab67fc400fa9e53aa4689d127f93f93
Ioana Tiplea <ioanatiplea94@gmail.com>
2022-08-30 11:01:26 +0300

    Improve Table rendering logic and readability

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
 import { AppDispatch, RootState } from '../../redux/store'
-import { fetchCountriesThunk } from '../../redux/slices/countriesSlice'
+import { Country, fetchCountriesThunk } from '../../redux/slices/countriesSlice'
 import TableHead from '../TableHead/TableHead'
 import TableRow from '../TableRow/TableRow'
 
@@ -14,22 +14,23 @@ function Table() {
     dispatch(fetchCountriesThunk())
   }, [dispatch])
 
+  const isSearching = countries.filteredItems.length > 0
+
+  const handleRenderRows = (countries: Country[]) => {
+    return countries.map((country) => (
+      <TableRow key={country.name.common} {...country} />
+    ))
+  }
+
   return (
     // Wrapping in div for overflow-auto to work
     <div className='px-10 py-10 overflow-auto xl:px-20'>
       <table className='border-collapse w-full'>
         <TableHead />
         <tbody>
-          {
-            countries.filteredItems.length > 0
-              ? /* eslint-disable */
-                countries.filteredItems.map((country) => (
-                  <TableRow key={country.name.common} {...country} />
-                ))
-              : countries.items.map((country) => (
-                  <TableRow key={country.name.common} {...country} />
-                )) /* eslint-enable */
-          }
+          {isSearching
+            ? handleRenderRows(countries.filteredItems)
+            : handleRenderRows(countries.items)}
         </tbody>
       </table>
     </div>
