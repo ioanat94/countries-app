8fe5c2b6256040a270154aeba0d220b707faacba
Ioana Tiplea <ioanatiplea94@gmail.com>
2022-09-06 09:13:19 +0300

    Refactor nested ternary operator

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -43,6 +43,10 @@ function Table() {
     ? Math.ceil(countries.filteredItems.length / countriesPerPage)
     : Math.ceil(countries.items.length / countriesPerPage)
 
+  const loadingCountries = countries.isLoading
+  const loadedAndUserSearching = !loadingCountries && isSearching
+  const loadedAndUserNotSearching = !loadingCountries && !isSearching
+
   return (
     <div className='px-10 py-10 flex flex-col gap-6 xl:px-20'>
       {/* Wrapping in div for overflow-auto to work */}
@@ -50,13 +54,12 @@ function Table() {
         <table className='border-collapse w-full'>
           <TableHead />
           <tbody>
-            {countries.isLoading ? (
+            {loadingCountries && (
               <SkeletonTable countriesPerPage={countriesPerPage} />
-            ) : isSearching ? (
-              handleRenderRows(currentFilteredCountries)
-            ) : (
-              handleRenderRows(currentCountries)
             )}
+            {loadedAndUserSearching &&
+              handleRenderRows(currentFilteredCountries)}
+            {loadedAndUserNotSearching && handleRenderRows(currentCountries)}
           </tbody>
         </table>
       </div>
